087e2ece5546f5a055eaabd35e5f112933f27c90
sara saad <sara.saad@etu.u-pec.fr>
2022-03-12 01:42:09 +0100

    pull

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -3,15 +3,15 @@ import { useDispatch } from "react-redux";
 import { login } from "../../redux/apiCalls";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
 
   const handleClick = (e) => {
     e.preventDefault();
-    login(dispatch, { username, password });
+    login(dispatch, { email: email, password });
   };
-
+  
   return (
     <div
       style={{
@@ -25,8 +25,8 @@ const Login = () => {
       <input
         style={{ padding: 10, marginBottom: 20 }}
         type="text"
-        placeholder="username"
-        onChange={(e) => setUsername(e.target.value)}
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         style={{ padding: 10, marginBottom: 20 }}

732a5cb6b104b459efcde12e051eecf6e07c019a
sara saad <sara.saad@etu.u-pec.fr>
2022-03-23 00:40:57 +0100

    main

--- api/routes/product.js
+++ api/routes/product.js
@@ -3,19 +3,13 @@ const { verifyTokenAndAdmin } = require("../middlewares/verifyToken");
 const {createProduct, updateProduct, deleteProduct, getAllProducts,getProduct} = require("../controllers/products");
 
 const router = require("express").Router();
-/*
+
 router.post("/", verifyTokenAndAdmin, createProduct);
 router.put("/:id", verifyTokenAndAdmin, updateProduct);
 router.delete("/:id", verifyTokenAndAdmin, deleteProduct);
 router.get("/find/:id", getProduct);
 router.get("/", getAllProducts);
-*/
 
-router.post("/", createProduct);
-router.put("/:id",  updateProduct);
-router.delete("/:id",  deleteProduct);
-router.get("/find/:id", getProduct);
-router.get("/", getAllProducts);
 
 
 module.exports = router;
--- api/routes/user.js
+++ api/routes/user.js
@@ -4,17 +4,11 @@ const {updateUser, deleteUser, getUser, getAllUsers, getUserStats} = require("..
 const {getAllOrders} = require("../controllers/order");
 
 const router = require("express").Router();
-/*
+
 router.put("/:id", verifyTokenAndAuthorization, updateUser);
 router.delete("/:id", verifyTokenAndAuthorization, deleteUser);
 router.get("/find/:id", verifyTokenAndAdmin, getUser);
 router.get("/", verifyTokenAndAdmin, getAllUsers);
-router.get("/stats", verifyTokenAndAdmin,getUserStats); */
-
-router.put("/:id",updateUser);
-router.delete("/:id", deleteUser);
-router.get("/find/:id", getUser);
-router.get("/", getAllUsers);
-router.get("/stats", getUserStats);
+router.get("/stats", verifyTokenAndAdmin,getUserStats); 
 
 module.exports = router;
